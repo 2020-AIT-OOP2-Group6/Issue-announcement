ffd03769c73588385704672b9b93a44e1007835b
happy663 <tatu2425@gmail.com>
2020-12-11 19:22:11 +0900

    名前と一時的なスコアをランキングに追加を行う処理を書いた

--- app/play.py
+++ app/play.py
@@ -12,6 +12,28 @@ import json
 module_play = Blueprint('play', __name__)
 
 
-@module_play.route("/play")
+@module_play.route("/play", method=['POST'])
 def play():
-    print('hello')
+
+    player_name = request.args.get('pname', None)
+
+    # dict型のデータ
+    json_dict = {"pname": p_name, "score": 0}
+
+    with open('score.json') as fr:
+
+        # dict型
+        json_data = json.load(fr)
+
+        # dict型のリストに追加
+        json_data.append(json_dict)
+
+        # json文字列に変換
+        json_string = json.dump(json_data, indent=4)
+
+        with open('score.json') as fw:
+            fw.write(json_string)
+
+    return jsonify({
+        "message": "名前追加"
+    })
