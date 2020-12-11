9bdcf52759c98117b0719e746b0f72ada49fefa6
r121212-Iwama <75057420+r121212-Iwama@users.noreply.github.com>
2020-12-11 20:48:13 +0900

    test.py消しました

--- test.py
+++
@@ -1,8 +0,0 @@
-
-
-scores = [{'player_name': 'suzuki tarou', 'score': 1500}, {'player_name': 'tanaka zirou',
-                                                           'score': 1800}, {'player_name': 'yamaguti kakeru', 'score': 1300}]
-scores_sorted = sorted(scores, key=lambda x: x['score'], reverse=True)
-
-
-print(scores_sorted)
