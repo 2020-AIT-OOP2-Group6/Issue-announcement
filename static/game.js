9f566272f8aae53b78c974383a15e50108d9b701
fuga-h <k19094kk@aitech.ac.jp>
2020-12-24 01:46:50 +0900

    カード交換の動き完成

--- static/game.js
+++ static/game.js
@@ -1,6 +1,20 @@
-document.getElementById("battle").onclick = function() {
-    
-};
+// function result_show(){
+//     result.className = gameset;
+// }
+
+// document.getElementById("battle").onclick = function() {
+//     alert(a);
+//     //comの手札開示
+//     // document.getElementById("com00").src = static/tranp_img/ClubsA.png;
+//     // document.getElementById("com01").src = tmp;
+//     // document.getElementById("com02").src = tmp;
+//     // document.getElementById("com03").src = tmp;
+//     // document.getElementById("com04").src = tmp;
+
+//     // setTimeout(result_show, 1000);
+// };
+
+
 
 function choose_card(){
     count = 0;
@@ -70,7 +84,6 @@ function choose_card(){
         }
     };
     
-    
     document.getElementById("hand04").onclick = function() {
         hand04 = document.getElementById("hand04");
         if(count<3){
@@ -88,8 +101,72 @@ function choose_card(){
     };
 };
 
-document.getElementById("card_change").onclick = function() {
-    
+function turn_background(){
+    document.getElementById('card_change').disabled = true;
+    if(hand00.className == "active"){
+        tmp0 = document.getElementById("hand00").src
+        document.getElementById("hand00").src =　"static/card_back.png";
+    }
+    if(hand01.className == "active"){
+        tmp1 = document.getElementById("hand01").src
+        document.getElementById("hand01").src =　"static/card_back.png";
+    }
+    if(hand02.className == "active"){
+        tmp2 = document.getElementById("hand02").src
+        document.getElementById("hand02").src =　"static/card_back.png";
+    }
+    if(hand03.className == "active"){
+        tmp3 = document.getElementById("hand03").src
+        document.getElementById("hand03").src =　"static/card_back.png";
+    }
+    if(hand04.className == "active"){
+        tmp4 = document.getElementById("hand04").src
+        document.getElementById("hand04").src =　"static/card_back.png";
+    }
 };
 
-choose_card();
+function newcard_get(){
+    if(hand00.className == "active"){
+        document.getElementById("hand00").src =　tmp0;//tmpに新しいカード渡す
+    }
+    if(hand01.className == "active"){
+        document.getElementById("hand01").src =　tmp1;//tmpに新しいカード渡す
+    }
+    if(hand02.className == "active"){
+        document.getElementById("hand02").src =　tmp2;//tmpに新しいカード渡す
+    }
+    if(hand03.className == "active"){
+        document.getElementById("hand03").src =　tmp3;//tmpに新しいカード渡す
+    }
+    if(hand04.className == "active"){
+        document.getElementById("hand04").src =　tmp4;//tmpに新しいカード渡す
+    }
+    
+  
+}
+
+function className_normal(){
+    hand00.className = "";
+    hand01.className = "";
+    hand02.className = "";
+    hand03.className = "";
+    hand04.className = "";
+}
+
+
+function card_change(){
+
+    if(document.getElementById('card_change').disabled == false){
+        choose_card();
+    }
+    document.getElementById("card_change").onclick = function() {
+        if(hand00.className == "active" || hand01.className == "active" || hand02.className == "active" || hand03.className == "active" || hand04.className == "active"){
+            turn_background();
+            setTimeout(newcard_get, 1000);
+            setTimeout(className_normal,1500);
+        }
+    };
+
+};
+
+card_change();
