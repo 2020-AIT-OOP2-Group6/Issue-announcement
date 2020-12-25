7e7e6795adae04adc8b424e27547737c5f9430c1
r121212-Iwama <75057420+r121212-Iwama@users.noreply.github.com>
2020-12-25 20:49:53 +0900

    debug用の手札判定
    デバッグしてみて、おかしいなって手があったらWeb.pyのterminal上の自分の手と相手の手をコピーしてtert.pyで試してください。それでもおかしい場合は、報告してください

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-
+import Type_Adjust
+import Compare
 
 list = []
 
@@ -11,3 +12,28 @@ for i, target_list in enumerate(list):
 
 
 print(list)
+
+hand_list = ['Joker', 'HeartsA', 'SpadesA', 'DiamondsQ', 'HeartsQ']
+ophand_list = ['DiamondsK', 'HeartsK', 'ClubsK', 'SpadesJ', 'DiamondsJ']
+hand_dictionary = []
+for hand in hand_list:
+    hand_dictionary.append(Type_Adjust.Adjust(hand))
+
+print(hand_dictionary)
+
+ophand_dictionary = []
+for ophand in ophand_list:
+    ophand_dictionary.append(Type_Adjust.Adjust(ophand))
+
+print(ophand_dictionary)
+
+# 外部クラスのインスタンス
+coh = Compare.CompareHand()
+
+# 勝敗判断
+judge, hand_score = coh.judge_card(hand_dictionary, ophand_dictionary)
+
+print(judge,hand_score)
+        
+
+
